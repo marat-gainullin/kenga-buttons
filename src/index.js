b8931fb2e9b5da824e3fd4ffc411242ad076342a
marat-gainullin <mg@altsoft.biz>
2017-10-01 00:50:34 +0300

    NPM compliance 2

---
+++ src/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Button: require('./button'),
+    CheckBox: require('./check-box'),
+    DropDownButton: require('./drop-down-button'),
+    RadioButton: require('./radio-button'),
+    ToggleButton: require('./toggle-button')
+};
